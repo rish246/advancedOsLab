b3504f347dee86ae3decb00bc0767814be16b96d
rish246 <rishabhkatna2228@gmail.com>
2020-09-03 14:24:07 +0530

    refactor python fcfs code

--- practical1/fcfs_python.py
+++ practical1/fcfs_python.py
@@ -1,3 +1,4 @@
+##############################################################################################################################################
 # create a class named Job
 class Job:
 
@@ -11,8 +12,8 @@ class Job:
     def print_stats(self):
         TAB = "\t\t"
         print(f'{self.name.center(10)}\t|\t{self.arrival_time}\t|\t{self.burst_time}\t|\t{self.waiting_time}\t|\t{self.completion_time}')
-
-
+################################################################################################################################################
+##############################################################################################
 # function to insert jobs in queue
 def insert_jobs_in_queue(ready_queue, names_of_jobs, arrival_times, burst_times):
     number_of_jobs = len(names_of_jobs)
@@ -30,10 +31,60 @@ def insert_jobs_in_queue(ready_queue, names_of_jobs, arrival_times, burst_times)
         ready_queue.append(new_job)
 
     return ready_queue
+#################################################################################################
+
+
+##################################################################################################
+# function that will process each job in the ready queue 
+# returns queue of finished jobs(in particular order)
+# total_processing_time => time it took to process all the jobs in ready queue
+# total_waiting_time => total waiting time of the system
+def process_jobs(ready_queue):
+    finished_jobs = []
+
+    total_processing_time = 0
+    total_waiting_time = 0
+
+
+    # take each job from ready queue
+    # set its waiting time and completion time values
+    
+    for job in ready_queue:
+        
+        # update the values of waiting times and completion time
+        job.waiting_time = total_processing_time < job.arrival_time and 0 or (total_processing_time - job.arrival_time)
+
+        total_waiting_time += job.waiting_time
+
+        job.completion_time = job.arrival_time + job.waiting_time + job.burst_time
+
+        total_processing_time = job.completion_time
+
+        finished_jobs.append(job)
+
+    
+    return finished_jobs, total_processing_time, total_waiting_time
+########################################################################################################
+
+#####################################################################################
+# function that will print the table and final information related to the algorithm
+
+def print_final_information(finished_jobs, average_waiting_time, total_processing_time):
+    print('----------------------------------------------------------------------------')    
+    print(f'  Name\t\t\tA.T\t\tB.T\t\tW.T\t\tC.T')
+    print('----------------------------------------------------------------------------')
+    for job in finished_jobs:
+        job.print_stats()
+    print('----------------------------------------------------------------------------')
+
+    print(f'average waiting time in the system  = {average_waiting_time}')
+    print(f'total time took to complete all the jobs = {total_processing_time}')
+########################################################################################
 
 
 
-
+########################################################################################
+############################ main function ############################################# 
 def main():
     # information of the jobs
     names_of_jobs = ["MsWord", "Vscode", "Notepad", "Intellij", "Google Chrome"]
@@ -45,39 +96,14 @@ def main():
     ready_queue = insert_jobs_in_queue(ready_queue, names_of_jobs, arrival_times, burst_times)
 
     # store the finished jobs
-    finished_jobs = []
-
-    booked_slot = 0
-    total_waiting_time = 0
-
     
-    for job in ready_queue:
-        
-        # update the values of waiting times and completion time
-        job.waiting_time = booked_slot < job.arrival_time and 0 or (booked_slot - job.arrival_time)
-
-        total_waiting_time += job.waiting_time
-
-        job.completion_time = job.arrival_time + job.waiting_time + job.burst_time
-
-        booked_slot = job.completion_time
-
-        finished_jobs.append(job)
-
+    finished_jobs, total_processing_time, total_waiting_time = process_jobs(ready_queue)
     
     average_waiting_time = (total_waiting_time / len(names_of_jobs))
 
-
-    print('----------------------------------------------------------------------------')    
-    print(f'  Name\t\t\tA.T\t\tB.T\t\tW.T\t\tC.T')
-    print('----------------------------------------------------------------------------')
-    for job in finished_jobs:
-        job.print_stats()
-    print('----------------------------------------------------------------------------')
-
-    print(f'average waiting time in the system  = {average_waiting_time}')
-    print(f'total time took to complete all the jobs = {booked_slot}')
-
+    print_final_information(finished_jobs, average_waiting_time, total_processing_time)
+    
+###############################################################################################
 
 main()
 
