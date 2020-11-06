dc2b0d23d13c45e4917bfd3af3fc5a538aa39e69
rish246 <rishabhkatna2228@gmail.com>
2020-11-06 09:17:26 +0530

    refactored client and server code

--- practical5/client.cpp
+++ practical5/client.cpp
@@ -12,26 +12,26 @@ using namespace std;
 
 const int PORT = 5400;
 
-int main()
+int connect_to_server(int PORT, in_addr_t IP)
 {
-    // calculate time now
-    time_t T0 = chrono::system_clock::to_time_t(chrono::system_clock::now());
-    cout << "Original time of the client clock : " << ctime(&T0) << endl;
-
-    // initialize socket
     int client_socket = socket(AF_INET, SOCK_STREAM, 0);
 
     sockaddr_in server_address{};
     server_address.sin_port = htons(PORT);
     server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_addr.s_addr = IP;
 
     // connect
-    if(connect(client_socket, (sockaddr *)&server_address, sizeof(server_address)) == -1){
+    if (connect(client_socket, (sockaddr *)&server_address, sizeof(server_address)) == -1)
+    {
         cerr << "Couldn't connect to server" << endl;
     }
 
-    // send empty message
+    return client_socket;
+}
+
+time_t get_correct_time(int client_socket, time_t T0)
+{
     int message_code = SEND_CORRECT_TIME;
 
     // send this message code to server
@@ -40,19 +40,33 @@ int main()
     // recv Ts timestamp from server
     int Ts;
     recv(client_socket, &Ts, sizeof(Ts), 0);
-    cout << "Delay occured = " << Ts << endl;
+    cout << "Server Time = " << Ts << endl;
     // compute T1
+
     auto T1 = chrono::system_clock::to_time_t(chrono::system_clock::now());
-    // calculate Tp
+
     auto round_trip_time = (T1 - T0);
 
-    time_t Tp = ((double) round_trip_time / 2) + Ts;
+    time_t Tp = ((double)round_trip_time / 2) + Ts;
 
     cout << "Round trip time of the message :\t" << round_trip_time << endl;
+
+    return Tp;
+}
+
+int main()
+{
+    // calculate time now
+    time_t T0 = chrono::system_clock::to_time_t(chrono::system_clock::now());
+    cout << "Original time of the client clock : " << ctime(&T0) << endl;
+
+    // initialize socket
+    int client_socket = connect_to_server(PORT, INADDR_ANY);
+
+    time_t Tp = get_correct_time(client_socket, T0);
+
     cout << "Correct time of the clock :\t" << ctime(&Tp) << endl;
 
     fflush(stdout);
     close(client_socket);
-
-
 }
--- practical5/server.cpp
+++ practical5/server.cpp
@@ -12,16 +12,15 @@ using namespace std;
 
 const int PORT = 5400;
 
-int main()
+int start_server(int PORT, in_addr_t IP)
 {
-    // bind
     int server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
     // struct
     sockaddr_in server_address{};
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(PORT);
-    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_addr.s_addr = IP;
 
     // bind
     bind(server_socket, (sockaddr *)&server_address, sizeof(server_address));
@@ -30,26 +29,48 @@ int main()
     listen(server_socket, 5);
 
     printf("Listening at port %d\n", PORT);
+
+    return server_socket;
+}
+
+time_t get_utc_time(time_t T)
+{
+    tm *utc_time{};
+    // Ts has cur time
+    time(&T);
+
+    utc_time = gmtime(&T);
+
+    return mktime(utc_time);
+}
+
+void server_client(int client_socket)
+{
+    int request_code;
+    recv(client_socket, &request_code, sizeof(int), 0);
+    cout << "Request Code  : " << request_code << endl;
+    if (request_code == SEND_CORRECT_TIME)
+    {
+        // change this to get utc time
+        time_t Ts = 0;
+        Ts = get_utc_time(Ts);
+        cout << Ts << endl;
+
+        // To introduct some round trip time
+        sleep(Ts % 3);
+        send(client_socket, &Ts, sizeof(Ts), 0);
+    }
+}
+
+int main()
+{
+    // bind
+    int server_socket = start_server(PORT, INADDR_ANY);
     // recieve connection
     while (true)
     {
         int client_socket = accept(server_socket, nullptr, nullptr);
-
-        int request_code;
-        recv(client_socket, &request_code, sizeof(int), 0);
-        cout << "Request Code  : " << request_code << endl;
-        if(request_code == SEND_CORRECT_TIME){
-
-            // introduce a random delay (bw 0, 100)
-            // get time of cur
-            time_t Ts = chrono::system_clock::to_time_t(chrono::system_clock::now());
-            cout << Ts << endl;
-            sleep(Ts % 10);
-            send(client_socket, &Ts, sizeof(Ts), 0);
-
-        }
-
+        server_client(client_socket);
         close(client_socket);
     }
-
 }
